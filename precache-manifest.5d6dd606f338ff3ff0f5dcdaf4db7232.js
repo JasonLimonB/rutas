67846f08fc5a04481ee41c4aa83bedaa34641b37
Jason Limon <jasong.limonb@gmail.com>
2019-01-29 23:43:49 -0600

    Updates

---
+++ precache-manifest.5d6dd606f338ff3ff0f5dcdaf4db7232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56ee0a3bdceda050bf5a",
+    "url": "/rutas/static/js/runtime~main.56ee0a3b.js"
+  },
+  {
+    "revision": "76d45d754d175bd68617",
+    "url": "/rutas/static/js/main.76d45d75.chunk.js"
+  },
+  {
+    "revision": "ea952d85a93042d428c3",
+    "url": "/rutas/static/js/1.ea952d85.chunk.js"
+  },
+  {
+    "revision": "76d45d754d175bd68617",
+    "url": "/rutas/static/css/main.9aa51771.chunk.css"
+  },
+  {
+    "revision": "5633855117220d6625624e5822c2e7b3",
+    "url": "/rutas/index.html"
+  }
+];
